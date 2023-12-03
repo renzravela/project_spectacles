da8075b906b9babd0e72c4c378ef5eb25ca29dea
Elghene <elghenemaglaque@gmail.com>
2023-12-03 08:54:41 +0800

    elghene_b_sunday8

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -10,20 +10,31 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    
+
     {{-- Sweet Alerts --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <style>
+        .movie-image {
+    width: 200px;
+    height: 300px;
+    margin-left: 1rem;
+    border-radius: 2rem;
+    margin-bottom: 2rem;
+    /* Add any additional styling here */
+}
+    </style>
 </head>
 <body class="bg-dark bg-opacity-50" >
     <nav class="navbar navbar-expand-lg bg-dark">
         <div class="container">
             <a class="navbar-brand text-light" href="#">Spectikol</a>
-            
+
             <!-- Toggle button for small screens -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-    
+
             <!-- Navbar links and search form -->
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
@@ -37,13 +48,13 @@
                         <a class="nav-link text-light" href="#">TV Shows</a>
                     </li>
                 </ul>
-    
+
                 <!-- Search form -->
                 <form class="d-flex ms-auto mx-auto"> <!-- Added mx-auto for centering -->
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-secondary text-light" type="submit">Search</button>
                 </form>
-    
+
                 <!-- Display user information or login/register link -->
                 @if (session('user_id'))
                     <span class="navbar-text text-light mr-auto" style="width: 100px">Hello, {{ session('user_name') }}</span>
@@ -55,21 +66,20 @@
             </div>
         </div>
     </nav>
-    
+
     <div class="container mt-4">
         <h1>Movie List</h1>
-    
-        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+
+        <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3">
             @foreach ($movie_list as $movie)
                 <div class="col-md-4 mb-4">
                     <div class="card bg-dark bg-opacity-75 text-light">
                         <div class="card-body">
-                            <img src="../../images/rush_2013.jpg" class="image" alt="">
+                            <img src="{{ asset('storage/' . $movie->image) }}" alt="{{ $movie->title }} Image" class="movie-image">
                             <h5 class="card-title">{{ $movie->title }}</h5>
-                            <p class="card-text">{{ $movie->director }}</p>
-                            <p class="card-text">{{ $movie->genre }}</p>
-                            <p class="card-text">{{ $movie->description }}</p>
-                            <p class="card-text">{{ $movie->year_release }}</p>
+                            {{-- <p class="card-text">{{ $movie->director }}</p> --}}
+                            <p class="card-text">{{ $movie->genre }} | {{ $movie->year_release }}</p>
+                            {{-- <p class="card-text">{{ $movie->description }}</p> --}}
                         </div>
                     </div>
                 </div>
