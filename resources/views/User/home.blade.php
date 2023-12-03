81055a00426c702c22c3660e409ea1600711250b
Imteitei <basingakenneth@gmail.com>
2023-12-03 13:43:51 +0800

    sheesh

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Spectacles Movies PH</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <!-- Bootstrap and custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> --}}
 
     {{-- Sweet Alerts --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
